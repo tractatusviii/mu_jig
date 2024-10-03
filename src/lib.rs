0133f08578401043d5d3c031a9bea6f8cb28df3e
rtviii <rtkushner@gmail.com>
2024-10-03 00:19:02 -0500

    coming back to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,127 @@
-
 use nom::{
-    combinator::map_res,
+    bytes::complete::tag, character::complete::{alphanumeric1, digit1, space1}, combinator::{map_res, opt}, multi::count, number::complete::float, sequence::{preceded, tuple}, IResult
 };
 
-
-const STRING: &str = "Hello, World!";
-
-const BLOCK: &str = "loop_
-_atom_site.group_PDB 
-_atom_site.id 
-_atom_site.type_symbol 
-_atom_site.label_atom_id 
-_atom_site.label_alt_id 
-_atom_site.label_comp_id 
-_atom_site.label_asym_id 
-_atom_site.label_entity_id 
-_atom_site.label_seq_id 
-_atom_site.pdbx_PDB_ins_code 
-_atom_site.Cartn_x 
-_atom_site.Cartn_y 
-_atom_site.Cartn_z 
-_atom_site.occupancy 
-_atom_site.B_iso_or_equiv 
-_atom_site.pdbx_formal_charge 
-_atom_site.auth_seq_id 
-_atom_site.auth_comp_id 
-_atom_site.auth_asym_id 
-_atom_site.auth_atom_id 
-_atom_site.pdbx_PDB_model_num 
-ATOM   1     N N     . ALA A  1  2    ? 185.986 228.605 206.762 1.00 4.58   ? 1    ALA 0 N     1 
-ATOM   2     C CA    . ALA A  1  2    ? 185.505 229.088 208.055 1.00 4.27   ? 1    ALA 0 CA    1 
-ATOM   3     C C     . ALA A  1  2    ? 184.672 230.300 207.814 1.00 2.40   ? 1    ALA 0 C     1 
-ATOM   4     O O     . ALA A  1  2    ? 185.159 231.267 207.269 1.00 3.02   ? 1    ALA 0 O     1 
-ATOM   5     C CB    . ALA A  1  2    ? 186.651 229.422 208.997 1.00 4.30   ? 1    ALA 0 CB    1 
-ATOM   6     N N     . VAL A  1  3    ? 183.426 230.259 208.246 1.00 1.99   ? 2    VAL 0 N     1";
-
-pub fn count() -> i32 {
-    for (index, line) in BLOCK.lines().enumerate() {
-        println!("Line {}: {}", index + 1, line);
-    }
-    0
-
-
-
-
+#[derive(Debug)]
+struct AtomSite {
+    group_pdb         : String,
+    id                : u32,
+    type_symbol       : String,
+    label_atom_id     : String,
+    label_alt_id      : String,
+    label_comp_id     : String,
+    label_asym_id     : String,
+    label_entity_id   : String,
+    label_seq_id      : Option<u32>,
+    pdbx_pdb_ins_code : String,
+    cartn_x           : f32,
+    cartn_y           : f32,
+    cartn_z           : f32,
+    occupancy         : f32,
+    b_iso_or_equiv    : f32,
+    pdbx_formal_charge: String,
+    auth_seq_id       : u32,
+    auth_comp_id      : String,
+    auth_asym_id      : String,
+    auth_atom_id      : String,
+    pdbx_pdb_model_num: u32,
 }
+
+fn parse_group_pdb(input: &str) -> IResult<&str, String> {
+    map_res(alphanumeric1, |s: &str| Ok::<_, ()>(s.to_string()))(input)
+}
+
+fn parse_id(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
+
+fn parse_string(input: &str) -> IResult<&str, String> {
+    map_res(alphanumeric1, |s: &str| Ok::<_, ()>(s.to_string()))(input)
+}
+
+fn parse_optional_u32(input: &str) -> IResult<&str, Option<u32>> {
+    opt(map_res(digit1, |s: &str| s.parse::<u32>()))(input)
+}
+
+// fn parse_spaced_item<F, O>(mut parser: F) -> impl FnMut(&str) -> IResult<&str, O>
+// where
+//     F: FnMut(&str) -> IResult<&str, O>,
+// {
+//     move |input: &str| { preceded(space1, &mut parser)(input) }
+// }
+
+
+// fn parse_atom_site(input: &str) -> IResult<&str, AtomSite> {
+//     let (input, group_pdb)                   = parse_group_pdb(input)?;
+//     let (input, id)                          = pa    // match parse_atom_site(input) {
+    //     Ok((_, atom_site)) => println!("Parsed AtomSite: {:#?}", atom_site),
+    //     Err(e) => println!("Error parsing input: {:?}", e),
+    // }rse_spaced_item(parse_id)(input)?;
+//     let (input, type_symbol)                 = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_atom_id)               = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_alt_id)                = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_comp_id)               = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_asym_id)               = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_entity_id)             = parse_spaced_item(parse_string)(input)?;
+//     let (input, label_seq_id)                = parse_spaced_item(parse_optional_u32)(input)?;
+//     let (input, pdbx_pdb_ins_code)           = parse_spaced_item(parse_string)(input)?;
+//     let (input, (cartn_x, cartn_y, cartn_z)) = parse_spaced_item(tuple(( float, parse_spaced_item(float), parse_spaced_item(float), )))(input)?;
+//     let (input, occupancy)                   = parse_spaced_item(float)(input)?;
+//     let (input, b_iso_or_equiv)              = parse_spaced_item(float)(input)?;
+//     let (input, pdbx_formal_charge)          = parse_spaced_item(parse_string)(input)?;
+//     let (input, auth_seq_id)                 = parse_spaced_item(parse_id)(input)?;
+//     let (input, auth_comp_id)                = parse_spaced_item(parse_string)(input)?;
+//     let (input, auth_asym_id)                = parse_spaced_item(parse_string)(input)?;
+//     let (input, auth_atom_id)                = parse_spaced_item(parse_string)(input)?;
+//     let (input, pdbx_pdb_model_num)          = parse_spaced_item(parse_id)(input)?;
+
+//     Ok((input, AtomSite {
+//         group_pdb,
+//         id,
+//         type_symbol,
+//         label_atom_id,
+//         label_alt_id,
+//         label_comp_id,
+//         label_asym_id,
+//         label_entity_id,
+//         label_seq_id,
+//         pdbx_pdb_ins_code,
+//         cartn_x,
+//         cartn_y,
+//         cartn_z,
+//         occupancy,
+//         b_iso_or_equiv,
+//         pdbx_formal_charge,
+//         auth_seq_id,
+//         auth_comp_id,
+//         auth_asym_id,
+//         auth_atom_id,
+//         pdbx_pdb_model_num,
+//     }))
+// }
+
+
+fn parse_input(input: &str) -> IResult<&str, &str> {
+    //  note that this is really creating a function, the parser for abc
+    //  vvvvv 
+    //         which is then called here, returning an IResult<&str, &str>
+    //         vvvvv
+    tag("ATOM")(input)
+}
+
+fn main() {
+
+    let input = "ATOM   1     N N     . ALA A  1  2    ? 185.986 228.605 206.762 1.00 4.58   ? 1    ALA 0 N     1 ";
+
+
+    match parse_input(input){
+        Err(e) => println!("Error parsing input: {:?}", e),
+        Ok((_, _)) => println!("Parsed input"),
+    };
+    ()
+    // match parse_atom_site(input) {
+    //     Ok((_, atom_site)) => println!("Parsed AtomSite: {:#?}", atom_site),
+    //     Err(e) => println!("Error parsing input: {:?}", e),
+    // }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
 use mu_jig;
 use pdbtbx;
 
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alphanumeric1, digit1, space1},
+    combinator::{map, map_res, opt},
+    multi::count,
+    number::complete::float,
+    sequence::{preceded, tuple},
+    IResult,
+};
 
+#[derive(Debug, PartialEq,Clone,Copy)]
+enum EntryType {
+    MOLECULE,
+    ATOM,
+}
+
+
+impl std::fmt::Display for EntryType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EntryType::MOLECULE => write!(f, "MOLECULE"),
+            EntryType::ATOM => write!(f, "ATOM"),
+        }
+    }
+}
+
+fn parse_entry_type(input: &str) -> IResult<&str, EntryType> {
+    alt((
+        map(tag("MOLECULE"), |_| EntryType::MOLECULE),
+        map(tag("ATOM"), |_| EntryType::ATOM),
+    ))(input)
+}
 
 fn main() {
+    let input1 = "ATOM   1     N N     . ALA A  1  2    ? 185.986 228.605 206.762 1.00 4.58   ? 1    ALA 0 N     1 ";
+    let input2 = "MOLECULE   1     N N     . ALA A  1  2    ? 185.986 228.605 206.762 1.00 4.58   ? 1    ALA 0 N     1 ";
 
-  let x =   mu_jig::count();
-  println!("Count: {}", x);
+    match parse_entry_type(input1) {
+        Ok((remainder, entry_type)) => println!("Input1: {:?}, Remainder: {:?}", entry_type, remainder),
+        Err(e) => println!("Error parsing input1: {:?}", e),
+    }
 
+    match parse_entry_type(input2) {
+        Ok((remainder, entry_type)) => println!("Input2: {:?}, Remainder: {:?}", entry_type, remainder),
+        Err(e) => println!("Error parsing input2: {:?}", e),
+    }
 }
